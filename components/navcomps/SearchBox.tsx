40d5f3a569dd5355e135c93fd545810ca115382d
Natnael Amin <natnaelamin24@gmail.com>
2025-02-13 17:31:49 +0300

    some more typscript issues

--- components/navcomps/SearchBox.tsx
+++ components/navcomps/SearchBox.tsx
@@ -1,6 +1,6 @@
 import { Search } from 'lucide-react'
 import React from 'react'
-import {Dialog, DialogContent, DialogDescription, DialogHeader, DialogTitle, DialogTrigger,
+import {Dialog, DialogContent, DialogHeader, DialogTrigger,
   } from "@/components/ui/dialog"
   
 
